a431e496f46ed1fd3efb6c056191d30c4801ff6d
abc <abc@example.com>
2020-04-07 17:41:14 +0530

    Updates

---
+++ precache-manifest.852be738871cba58f53abbd95c43817a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc90b694bbc41f5b2b5b48a5e867b9e6",
+    "url": "/covid-19Dashboard/index.html"
+  },
+  {
+    "revision": "ffeee83393ce7d70a051",
+    "url": "/covid-19Dashboard/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7a291442ae8741696a31",
+    "url": "/covid-19Dashboard/static/js/2.acccff25.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid-19Dashboard/static/js/2.acccff25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffeee83393ce7d70a051",
+    "url": "/covid-19Dashboard/static/js/main.be1327b6.chunk.js"
+  },
+  {
+    "revision": "b275d8726766c4ce71b1",
+    "url": "/covid-19Dashboard/static/js/runtime-main.65ea6eee.js"
+  }
+]);
